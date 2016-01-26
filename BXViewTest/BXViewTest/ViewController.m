3fb44c72df294a939c95ea35b7b1afe4104a355a
xuebingxin <qq250799469@163.com>
2016-01-26 19:51:03 +0800

    animate action

--- BXViewTest/BXViewTest/ViewController.m
+++ BXViewTest/BXViewTest/ViewController.m
@@ -11,7 +11,9 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) UIImageView *myIV;
+@property (nonatomic, strong) UIImageView *nextIV;
 @property (nonatomic, assign) BOOL isRotate;
+@property (nonatomic, assign) BOOL isMyIVShow;
 
 @end
 
@@ -29,16 +31,34 @@
 }
 
 - (void)initUI{
-    UIButton *rotateBtn = [[UIButton alloc]initWithFrame:CGRectMake(30, 50, 100, 40)];
+    UIButton *rotateBtn = [[UIButton alloc]initWithFrame:CGRectMake(30, 50, 80, 40)];
     [rotateBtn setTitle:@"点击旋转" forState:UIControlStateNormal];
     [rotateBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     rotateBtn.backgroundColor = [UIColor yellowColor];
     [rotateBtn addTarget:self action:@selector(rotateView) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:rotateBtn];
     
+    UIButton *animateBtn = [[UIButton alloc]initWithFrame:CGRectMake(120, 50, 80, 40)];
+    [animateBtn setTitle:@"动画隐藏" forState:UIControlStateNormal];
+    [animateBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    animateBtn.backgroundColor = [UIColor yellowColor];
+    [animateBtn addTarget:self action:@selector(animateView) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:animateBtn];
+    
+    UIButton *transitionBtn = [[UIButton alloc]initWithFrame:CGRectMake(210, 50, 80, 40)];
+    [transitionBtn setTitle:@"视图转换" forState:UIControlStateNormal];
+    [transitionBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    transitionBtn.backgroundColor = [UIColor yellowColor];
+    [transitionBtn addTarget:self action:@selector(transitionView) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:transitionBtn];
+
     self.myIV = [[UIImageView alloc]initWithFrame:CGRectMake(30, 120, 100, 100)];
     self.myIV.backgroundColor = [UIColor redColor];
     [self.view addSubview:self.myIV];
+    
+    self.nextIV = [[UIImageView alloc]initWithFrame:CGRectMake(150, 120, 100, 100)];
+    self.nextIV.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.nextIV];
     
     self.isRotate = NO;
     
@@ -60,16 +80,67 @@
     myLayer.position = CGPointMake(CGRectGetMidX(viewBounds), CGRectGetMidY(viewBounds));
 }
 
+#pragma mark - ----action
 - (void)rotateView {
-    if (!self.isRotate) {
-        CGAffineTransform xform = CGAffineTransformMakeRotation(M_PI/4.0);
-        self.myIV.transform = xform;
-        self.isRotate = YES;
-    } else {
-        CGAffineTransform xform = CGAffineTransformMakeRotation(0);
-        self.myIV.transform = xform;
-        self.isRotate = NO;
-    }
+    [UIView animateWithDuration:1.0 animations:^{
+        if (!self.isRotate) {
+            CGAffineTransform xform = CGAffineTransformMakeRotation(M_PI/4.0);
+            self.myIV.transform = xform;
+            self.isRotate = YES;
+        } else {
+            CGAffineTransform xform = CGAffineTransformMakeRotation(0);
+            self.myIV.transform = xform;
+            self.isRotate = NO;
+        }
+    }];
+}
+
+- (void)animateView {
+    // 立即隐藏
+    [UIView animateWithDuration: 1.0
+                          delay: 0.0
+                        options: UIViewAnimationOptionCurveEaseIn
+                     animations: ^{
+                         self.myIV.alpha = 0.0;
+                     }
+                     completion: ^(BOOL finished){
+                         // 1秒后在显示
+                         [UIView animateWithDuration: 1.0
+                                               delay: 1.0
+                                             options: UIViewAnimationOptionCurveEaseOut
+                                          animations: ^{
+                                              self.myIV.alpha = 1.0;
+                                          }
+                                          completion:nil];
+                     }];
+}
+
+//-(void)transitionView{
+//    [UIView transitionWithView: self.myIV
+//                      duration: 1.0
+//                       options: UIViewAnimationOptionTransitionCurlUp
+//                    animations: ^{
+//                        self.myIV.hidden = YES;
+//                        self.nextIV.hidden = NO;
+//                    }
+//                    completion:^(BOOL finished){
+//                        UIImageView *temp = self.myIV;
+//                        self.myIV = self.nextIV;
+//                        self.nextIV = temp;
+//                    }];
+//}
+
+- (void)transitionView {
+    [UIView transitionFromView: (self.isMyIVShow ? self.myIV : self.nextIV)
+                        toView: (self.isMyIVShow ? self.nextIV : self.myIV)
+                      duration: 1.0
+                       options: (self.isMyIVShow ? UIViewAnimationOptionTransitionFlipFromRight :
+                                UIViewAnimationOptionTransitionFlipFromLeft)
+                    completion: ^(BOOL finished) {
+                        if (finished) {
+                            self.isMyIVShow = !self.isMyIVShow;
+                        }
+                    }];
 }
 
 #pragma mark - ----external display
